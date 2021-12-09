9883e9752eccdfb738ce7b54c5ec7540e86eea4f
Olivier02500 <olivier.peuchet@hotmail.com>
2021-12-09 20:15:14 +0100

    make exo 99

--- exercice1.js
+++ exercice1.js
@@ -1,12 +1,25 @@
 // Make it count up on a timer, calling this function
 let seconds = document.getElementById("seconds");
+let a = 0;
+let timeoutId = null;
+
 let countUp = function() {
-    // ?
+     timeoutId = setTimeout(function (){
+        seconds.innerText = a.toString();
+        a++;
+        countUp();
+        return timeoutId;
+    },1000)
 };
+countUp();
 
 // How can you make it stop counting?
+
 let stopCountUp = function() {
+    clearTimeout(timeoutId);
+    stopButton.removeEventListener("click", stopCountUp);
 };
+
 let stopButton = document.getElementById("stop-button");
 stopButton.addEventListener("click", stopCountUp);
 
